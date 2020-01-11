c4e934739c014bd3a314a96387c584e7eb87ef9b
takutoki <34600200+takutoki@users.noreply.github.com>
2020-01-11 15:24:45 +0900

    crate functional component, class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
-function App() {
-  return (
-    <React.Fragment>
-      <label htmlFor="bar">Bar!!!</label>
-      <input type="text" onChange={() => {console.log("I am clicked")}} />
-    </React.Fragment>
-  );
+// クラスコンポーネント
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <label>HogeFooBar!</label>
+        <Cat />
+      </React.Fragment>
+    )
+  }
 }
+
+// Functional Component (allow)
+const Cat = () => {
+  return <div>nyaa nyaa</div>
+}
+
+// Functional Component
+// function App() {
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar">Bar!!!</label>
+//       <input type="text" onChange={() => {console.log("I am clicked")}} />
+//     </React.Fragment>
+//   );
+// }
 
 export default App;
